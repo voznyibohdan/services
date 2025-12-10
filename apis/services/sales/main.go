bcedf72b6ee7ecae3e502a56d8224351ce2bb9f1
bohdanvoznyi <bohdanvoznyi.em@gmail.com>
2025-12-10 21:01:19 +0200

    commit 2

---
+++ apis/services/sales/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/voznyibohdan/services/foundation/logger"
+)
+
+func main() {
+	var log *logger.Logger
+
+	events := logger.Events{
+		Error: func(ctx context.Context, record logger.Record) {
+			log.Info(ctx, "****** SEND ALERT ******")
+		},
+	}
+
+	traceIDFn := func(ctx context.Context) string {
+		return "123456789"
+	}
+
+	log = logger.NewWithEvents(os.Stdout, logger.LevelInfo, "SALES", traceIDFn, events)
+
+	ctx := context.Background()
+
+	if err := run(ctx, log); err != nil {
+		log.Error(ctx, "startup", "msg", err.Error())
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, log *logger.Logger) error {
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-shutdown
+
+	log.Info(ctx, "shutdown", "status", "shutdown started", "signal", sig)
+	defer log.Info(ctx, "shutdown", "status", "shutdown complete", "signal", sig)
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/voznyibohdan/services
 
 go 1.25.4
+
+require github.com/go-json-experiment/json v0.0.0-20251027170946-4849db3c2f7e
